21d56eb657d2af94bb4113fe00f789b653109096
zx4580 <42490454+zx4580@users.noreply.github.com>
2019-07-08 15:58:01 +0800

    文章详情页面的组件
    文章详情页面的组件文章详情页面的组件文章详情页面的组件文章详情页面的组件

---
+++ components/post/paperleft.vue
@@ -0,0 +1,29 @@
+<template>
+<!-- 文章标题 -->
+<div>
+ <Papertitle />
+ <!-- 文章正文 -->
+ <Postcontent  />
+ <!-- 评论点赞收藏 -->
+ <Postcontrol />
+</div>
+</template>
+
+<script>
+import Papertitle from "@/components/post/papertitle";
+import Postcontent from "@/components/post/postcontent";
+import Postcontrol from "@/components/post/postcontrol"
+export default {
+  
+    components: {
+    Papertitle,
+    Postcontent,
+    Postcontrol
+  },
+
+};
+</script>
+
+<style>
+
+</style>
---
+++ components/post/paperright.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="right">
+          <div class="right-title">
+            <h3>相关攻略</h3>
+          </div>
+          <div class="right-content">
+
+          </div>
+        </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
